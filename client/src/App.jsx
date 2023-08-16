c237caafb75a4ef0042b2e846f5a551453727b9b
annazhutava <anna.zhutava@ecutbildning.se>
2023-08-17 01:38:40 +0200

    added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Product from "./routes/Product";
+import NotFound from "./routes/NotFound";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product/:id" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
