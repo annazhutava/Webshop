8c470001731ad43451709c9b31e2a28390bba17b
annazhutava <anna.zhutava@ecutbildning.se>
2023-08-17 19:52:05 +0200

    added links to navbar & shopping cart icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="navbar-left">
+          <div className="item">
+            <Link to="/products">Shop</Link>
+          </div>
+          <div className="item">
+            <Link to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Contact</Link>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <div className="item">
+            <Link to="/">Home</Link>
+          </div>
+        </div>
+        <div className="navbar-right">
+          <div className="cartIcon">
+            <ShoppingCartOutlinedIcon />
+            <span>0</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
