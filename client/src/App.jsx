dc456e5c02de3a86db7342475ca6f1f676c45ca7
annazhutava <anna.zhutava@ecutbildning.se>
2023-08-17 02:32:54 +0200

    added navbar & footer components along with "products" route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import Outlet from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,13 +6,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Product from "./routes/Product";
 import NotFound from "./routes/NotFound";
+import Products from "./routes/Products.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route indexelement={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="products" element={<Products />} />
           <Route path="product/:id" element={<Product />} />
           <Route path="*" element={<NotFound />} />
         </Route>
