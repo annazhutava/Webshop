506142eb001b28f95fcfda24b5ed2e804aca6759
annazhutava <anna.zhutava@ecutbildning.se>
2023-08-29 01:41:29 +0200

    added styling to 'about' page

--- client/src/routes/About/About.jsx
+++ client/src/routes/About/About.jsx
@@ -1,5 +1,36 @@
+import "../About/About.scss";
+import Image1 from "../../assets/about-img1.jpg";
+import Image2 from "../../assets/about-img2.jpg";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div className="top">
+        <div className="image">
+          <img src={Image1} alt="" />
+        </div>
+        <h1>About us</h1>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <div className="image">
+            <img src={Image2} alt="" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="about-description">
+            <h1>About</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, sequi
+              exercitationem voluptate, doloremque quisquam, expedita sed labore
+              consectetur ex saepe nobis minus unde numquam. Eaque illo adipisci
+              at alias aliquid.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
