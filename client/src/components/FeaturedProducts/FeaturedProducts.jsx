2cac36559348c30c2365a55ad409e14453afd922
annazhutava <anna.zhutava@ecutbildning.se>
2023-09-04 18:17:54 +0200

    created custom hook for data fetching & configured axios instance

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,53 +1,12 @@
 import { Link } from "react-router-dom";
-// import FeaturedProduct1 from "../../assets/featured-product-1.jpg";
-// import FeaturedProduct2 from "../../assets/featured-product-2.jpg";
-// import FeaturedProduct3 from "../../assets/featured-product-3.jpg";
 import Card from "../Card/Card";
 import "../FeaturedProducts/FeaturedProducts.scss";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-  // const data = [
-  //   {
-  //     id: 1,
-  //     img: FeaturedProduct1,
-  //   },
-  //   {
-  //     id: 2,
-  //     img: FeaturedProduct2,
-  //   },
-  //   {
-  //     id: 3,
-  //     img: FeaturedProduct3,
-  //   },
-  // ];
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          import.meta.env.VITE_REACT_APP_API_URL +
-            `/products?populate=*&[filters][type][$eq]=${type}`,
-          {
-            headers: {
-              Authorization:
-                "bearer " + import.meta.env.VITE_REACT_APP_API_TOKEN,
-            },
-          }
-        );
-
-        setData(res.data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  console.log(data);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
   return (
     <div className="featured-products">
@@ -59,9 +18,11 @@ const FeaturedProducts = ({ type }) => {
         </div>
       </div>
       <div className="right">
-        {data.map((item) => (
-          <Card key={item.id} item={item} />
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card key={item.id} item={item} />)}
       </div>
     </div>
   );
